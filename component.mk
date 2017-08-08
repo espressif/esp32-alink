f1ab8cafeb647d12ff84cab3371766e37a5638cb
zhanzhaochen <zhanzhaochen@espressif.com>
2017-08-08 21:39:04 +0800

    feat: add esp32 alink sds version

--- component.mk
+++ component.mk
@@ -2,12 +2,18 @@
 # Component Makefile
 #
 
-ifdef CONFIG_ALINK_EMBED_ENABLE
+ifdef CONFIG_ALINK_ENABLE
 
 COMPONENT_ADD_INCLUDEDIRS := include adaptation/include
 COMPONENT_SRCDIRS := adaptation application
 
+ifdef CONFIG_ALINK_VERSION_EMBED
 LIBS := $(dirname $(COMPONENT_PATH)) alink_agent tfspal
+endif
+
+ifdef CONFIG_ALINK_VERSION_SDS
+LIBS := $(dirname $(COMPONENT_PATH)) alink_agent_sds tfspal
+endif
 
 COMPONENT_ADD_LDFLAGS += -L $(COMPONENT_PATH)/lib \
                            $(addprefix -l,$(LIBS))
